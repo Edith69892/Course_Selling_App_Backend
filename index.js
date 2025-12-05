f102c934f4346e57a772cf732863e2f8b7dfc2c8
Roshan Patil <patilroshan9687@gmail.com>
2025-12-05 11:31:20 +0530

    add cors origin

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import app from "./app.js"
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
-mongoose.connect(`${process.env.MONGO_URI}${process.env.DB_NAME}`)
+dotenv.config({
+    path: "./.env"
+})
+
+;( async () => {
+
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}${process.env.DB_NAME}`)
+        console.log("sDatabase connected successfully")
+        app.listen(process.env.PORT || 5000, ()=>{
+            console.log("Server is running on port :", process.env.PORT);
+        })
+    } catch (error) {
+        console.log("MongoDB connection Failed:", error);
+    }
+    
+})();
+
+
 
 
